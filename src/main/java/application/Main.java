921ef0a5b0c3c9b4a9b7fe1d4b02b36fd4f8b313
Felipefams <augustofams@gmail.com>
2023-02-15 16:48:55 -0300

    "SellerDaoJDBC && DaoFactory"

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,14 +1,19 @@
 package application;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
+import model.dao.SellerDao;
 
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
+
         Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao selerDao = DaoFactory.createSellerDao(); //hides implementation from the main program
+        System.out.println("DEU");
         System.out.println(seller);
     }
 }
