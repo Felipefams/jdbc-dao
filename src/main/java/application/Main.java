90edd1dc73ceab287abd70b229bfb1776d99e69f
Felipefams <augustofams@gmail.com>
2023-02-15 19:29:16 -0300

    "Refactoring findById() for a cleaner code"

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -11,8 +11,8 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao(); //hides implementation from the main program
+        System.out.println("==== TEST  1: seller findByID ====");
         Seller seller = sellerDao.findById(3);
-        System.out.println("DEU");
         System.out.println(seller);
     }
 }
